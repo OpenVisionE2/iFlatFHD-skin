996f669189851247ba5e4b3a3148b9d90a2d96d0
persianpros <persianpros@yahoo.com>
2021-05-04 14:39:54 +0430

    Get rid of boxbranding, getBoxType and getBoxBrand

--- usr/lib/enigma2/python/Components/Converter/iFlatBoxInfo.py
+++ usr/lib/enigma2/python/Components/Converter/iFlatBoxInfo.py
@@ -5,7 +5,9 @@ from Components.Element import cached
 from Components.Sensors import sensors
 import os
 from Poll import Poll
-from enigma import getBoxType
+from Components.SystemInfo import BoxInfo
+
+model = BoxInfo.getItem("model")
 
 
 class iFlatBoxInfo(Poll, Converter, object):
@@ -39,7 +41,7 @@ class iFlatBoxInfo(Poll, Converter, object):
 
     def getModel(self):
         try:
-            box_info = getBoxType()
+            box_info = model
         except:
             return 'Model: N/A'
             box_info = None
@@ -108,7 +110,7 @@ class iFlatBoxInfo(Poll, Converter, object):
             return 'Uptime: %s' % uptime
 
     def getTempSensor(self):
-        if getBoxType() not in ("dm7020hd", "dm7020hdv2"):
+        if model not in ("dm7020hd", "dm7020hdv2"):
             try:
                 sensor_info = sensors.getSensorsList(sensors.TYPE_TEMPERATURE)
             except:
